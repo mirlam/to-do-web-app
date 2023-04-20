4d9348152d4da296af9c45367cfd6eeb0eab4be9
mirlam <mdirlam9@yahoo.com>
2023-04-19 20:48:17 -0500

    split out js

---
+++ src/views/to_do/delete.rs
@@ -0,0 +1,18 @@
+use crate::json_serialization::to_do_item::ToDoItem;
+use crate::processes::process_input;
+use crate::state::read_file;
+use crate::to_do::to_do_factory;
+use actix_web::{web, HttpResponse};
+use serde_json::value::Value;
+use serde_json::Map;
+
+pub async fn delete(to_do_item: web::Json<ToDoItem>) -> HttpResponse {
+    let state: Map<String, Value> = read_file("./state.json");
+    let title: String = to_do_item.title.clone();
+    let status: String = to_do_item.status.clone();
+    match to_do_factory(&status, title) {
+        Err(_item) => return HttpResponse::BadRequest().json(format!("{} not accepted", status)),
+        Ok(item) => process_input(item, String::from("delete"), &state),
+    }
+    return HttpResponse::Ok().json(return_state());
+}
