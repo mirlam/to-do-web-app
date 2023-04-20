f059ef60350c299fc2ea2512f449eb17d88c5b16
mirlam <mdirlam9@yahoo.com>
2023-04-19 20:48:03 -0500

    Added simple html content loader

--- src/views/app/items.rs
+++ src/views/app/items.rs
@@ -2,7 +2,9 @@ use super::content_loader::read_file;
 use actix_web::HttpResponse;
 
 pub async fn items() -> HttpResponse {
-    let html_data = read_file("./templates/main.html");
+    let mut html_data = read_file("./templates/main.html");
+    let javascript_data = read_file("./javascript/main.js");
+    html_data = html_data.replace("{{JAVASCRIPT}}", &javascript_data);
     HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(html_data)
--- src/views/to_do/mod.rs
+++ src/views/to_do/mod.rs
@@ -1,13 +1,29 @@
 use actix_web::web;
 mod create;
+mod delete;
+mod edit;
 mod get;
 mod utils;
-mod edit;
 use super::path::Path;
 
 pub fn item_factory(app: &mut web::ServiceConfig) {
-    let base_path: Path = Path { prefix: String::from("/item")};
-    app.route(&base_path.define(String::from("/create/{title}")),web::post().to(create::create));
-    app.route(&base_path.define(String::from("/get")), web::get().to(get::get));
-    app.route(&base_path.define(String::from("/edit")), web::put().to(edit::edit));
+    let base_path: Path = Path {
+        prefix: String::from("/item"),
+    };
+    app.route(
+        &base_path.define(String::from("/create/{title}")),
+        web::post().to(create::create),
+    );
+    app.route(
+        &base_path.define(String::from("/get")),
+        web::get().to(get::get),
+    );
+    app.route(
+        &base_path.define(String::from("/edit")),
+        web::put().to(edit::edit),
+    );
+    app.route(
+        &base_path.define(String::from("/delete")),
+        web::post().to(delete::delete),
+    );
 }
